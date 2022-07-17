e1353fc99ea780bc692ffa3aad1f26209059337f
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-07-17 17:59:36 +0200

    redt try 3

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 10:23:02 by yoav              #+#    #+#             */
-/*   Updated: 2022/07/17 12:57:24 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/07/17 17:59:13 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const char	*s;
 
 	if (!dest || !src)
-		return (NULL);
+		return (dest);
 	s = (const char *)src;
 	d = (char *)dest;
 	i = 0;
